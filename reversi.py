899f942f4a121a2b8e5f29f68c22a53a8d34683e
125621sr <84614501+125621sr@users.noreply.github.com>
2022-01-15 16:21:09 +0900

    Update reversi.py

--- reversi.py
+++ reversi.py
@@ -135,3 +135,21 @@ def my_AI(board, color): #おチビちゃんAI
     if put_and_reverse(board, position, color):
       return position
   return 0
+
+
+def point_AI(board, color):
+  point = [20,-10,0,0,-10,20,
+         -10,-5,-2,-2,-5,-10,
+         0,-2,0,0,-2,0,
+         0,-2,0,0,-2,0,
+         -10,-5,-2,-2,-5,-10,
+         20,-10,0,0,-10,20]
+  arr=[]
+  for pos, val in enumerate(point):
+    arr.append([val,pos])
+  arr.sort(reverse=True)
+  for i in range(len(arr)):
+    position = arr[i][1]
+    if put_and_reverse(board, position, color):
+      return position
+  return 0
