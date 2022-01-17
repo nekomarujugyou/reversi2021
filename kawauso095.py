d00c8c268917609a49468c8d8f4404b13b6cc414
Kimio Kuramitsu <kkuramitsu@gmail.com>
2022-01-17 16:10:58 +0900

    Update kawauso095.py

--- kawauso095.py
+++ kawauso095.py
@@ -168,8 +168,10 @@ def random_AI(board, color):
       return position ## おく位置を決めて返す
   return 0
 
+N = 6
 A=[5,30,0,35,3,17,2,18,12,23,32,33,13,27,22,26,16,19,8,9,4,34,31,1,11,6,24,29,10,25,28,7]
 def kawauso_AI(board, color):
+  global N
   for i in range(N*N):
     position =A[i]
     if put_and_reverse(board, position, color):
