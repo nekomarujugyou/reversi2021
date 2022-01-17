a6fd9ebe8d639c16efcdbf106c9445d586419ccc
myst7725 <94064325+myst7725@users.noreply.github.com>
2022-01-17 15:39:53 +0900

    Update green_AI038.py

--- green_AI038.py
+++ green_AI038.py
@@ -1,3 +1,89 @@
+import IPython
+from google.colab import output
+
+HTML = '''
+<style>
+#canvas {
+  background: lightgreen;
+}
+</style>
+<canvas id="canvas" width="320" height="320"></canvas>
+<script>
+var canvas = document.getElementById("canvas");
+var wsize = 320;
+var N=6;
+var dsize = (wsize / N);
+var r = (dsize/2);
+
+var board = [
+  0, 0, 0, 0, 0, 0, //           
+  0, 0, 0, 0, 0, 0, //           
+  0, 0, 1, 2, 0, 0, //           
+  0, 0, 2, 1, 0, 0, //           
+  0, 0, 0, 0, 0, 0, //           
+  0, 0, 0, 0, 0, 0, //           
+];
+
+function drawBoard(board) {
+  var context = canvas.getContext('2d');
+  for (var yi = 0; yi < N; yi += 1) {
+    for (var xi = 0; xi < N; xi += 1) {
+      if ((xi+yi) % 2 == 0) {
+        context.fillStyle = "darkgreen";
+        context.fillRect(xi*dsize, yi*dsize, dsize-1, dsize-1);       
+      }
+      if(board[yi*N+xi] != 0) {
+          context.beginPath () ;
+          var cx = xi*dsize + r;
+          var cy = yi*dsize + r;
+          context.arc( cx, cy, r, 0 * Math.PI / 180, 360 * Math.PI / 180, false ) ;
+          if(board[yi*N+xi] == 1) {
+            context.fillStyle = "rgba(0,0,0,1.0)" ;
+          }
+          else {
+            context.fillStyle = "rgba(255,255,255,1.0)" ;
+          }
+          context.fill() ;
+      }
+    }    
+  }    
+}
+
+drawBoard(board);
+
+
+function drawRect(x, y, width, height) {
+  var context = canvas.getContext('2d');
+  context.fillRect(x, y, width, height);
+}
+
+function onClick(e) {
+  console.log("click");
+  var x = e.clientX - canvas.offsetLeft;
+  var y = e.clientY - canvas.offsetTop;
+  //console.log("x:", x, "y:", y);
+  // 非同期でPython 側に送る
+  (async function() {
+    const result = await google.colab.kernel.invokeFunction('notebook.accept', [(x / dsize)|0, (y / dsize)|0], {});
+    const data = result.data['application/json'];
+    drawBoard(data.result);
+  })();
+  //drawRect(x, y, 10, 10);
+}
+
+canvas.addEventListener('click', onClick, false);
+</script>
+'''
+
+board = [
+  0, 0, 0, 0, 0, 0,           
+  0, 0, 0, 0, 0, 0,           
+  0, 0, 1, 2, 0, 0,           
+  0, 0, 2, 1, 0, 0,           
+  0, 0, 0, 0, 0, 0,           
+  0, 0, 0, 0, 0, 0,           
+]
+
 def green_AI(board,color):
   #リスト
   A=[0, 5, 30, 35, 
@@ -14,3 +100,28 @@ def green_AI(board,color):
         #置けるときは位置を返す
         return position
   return 0
+
+
+def run_reversi(ai):
+  display(IPython.display.HTML(HTML))
+  
+  def accept(x, y):
+    global count
+    count+=1
+    position=y*6+x
+    if count%2==0:
+      color=2
+      ai(board,color)
+      return IPython.display.JSON({'result': board})
+
+    else:
+      color=1
+      put_and_reverse(board, position, color)
+      print(count,":",x, y, y*6+x)
+      return IPython.display.JSON({'result': board})
+  
+
+  output.register_callback('notebook.accept', accept)
+  
+count=0
+run_reversi(green_AI)
